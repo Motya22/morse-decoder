65e6ec04a888d9192876304bb34d9c4658d64434
Motya22 <motya2222@gmail.com>
2022-03-12 21:26:44 +0300

    feat: done this task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  const wordsInNumbers =  expr.split('**********');
+  
+  const lettersInNumbers = wordsInNumbers.reduce((letters, word) => {
+  	const subarray = [];
+    
+  	for (let i = 0; i < word.length; i += 10) {
+      subarray.push(word.slice(i, i + 10));
+    }
+    
+    letters.push(subarray);
+    
+    return letters;
+  }, []);
+  
+  const lettersInDotsAndDashes = lettersInNumbers.map((word) => word.map((letterInNumber) => {
+  	let letterInDotsAndDashes = '';
+    
+    for (let i = 0; i < letterInNumber.length; i += 2) {
+      const twoNumbers = letterInNumber.slice(i, i + 2);
+      
+      if (twoNumbers === '10') {
+        letterInDotsAndDashes += '.';
+      } else if (twoNumbers === '11') {
+        letterInDotsAndDashes += '-';
+      }
+    }
+    
+    return letterInDotsAndDashes;
+  }));
+  
+  const letters = lettersInDotsAndDashes.map((word) => word.map((letter) => MORSE_TABLE[letter]));
+  
+  const words = letters.map((word) => word.join(''));
+  
+  return words.join(' ');
 }
 
 module.exports = {
